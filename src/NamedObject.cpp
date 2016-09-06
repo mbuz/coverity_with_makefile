0697294c6c73dff1571e6093e5372a54d2027c47
mbuz <mbuz@synopsys.com>
2016-09-06 08:26:54 +0200

    added boost serialization for compilation

--- src/NamedObject.cpp
+++ src/NamedObject.cpp
@@ -1,16 +1,20 @@
 #include "base.h"
 
-int test0 () {
-  int *i = NULL;
-  *i = *i +1;
-  return *i;
-}
-
 void NamedObject::setName(const string& bName) {
   name = bName;
 }
 
 const string& NamedObject::getName() const {
   return name;
+}
+
+// *****************************************************************************
+void NamedObject::store(boost::archive::text_oarchive& oa) {
+  oa << *this;
+}
+
+// *****************************************************************************
+void NamedObject::restore(boost::archive::text_iarchive& ia) {
+  ia >> *this;
 }
 
