07c49ff003602c3344dae1bdd9e5ee501fe3838c
mbuz <mbuz@synopsys.com>
2016-09-06 08:28:53 +0200

    added boost serialization for compilation

--- include/NamedObject.h
+++ include/NamedObject.h
@@ -1,3 +1,5 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
 
 // *****************************************************************************
@@ -10,10 +12,20 @@ public:
   NamedObject(const string& bName) :
     name(bName) {}
   
+  friend class  boost::serialization::access;
+  
+  template <typename Archive>
+  void serialize(Archive &ar,
+                 const unsigned int version) {
+    ar & name;
+  }  
+  
   void setName(const string& bName);
   const string& getName() const;
 
   virtual void print() const = 0;
+  virtual void store(boost::archive::text_oarchive& oa);
+  virtual void restore(boost::archive::text_iarchive& oa);
 };
 
 
